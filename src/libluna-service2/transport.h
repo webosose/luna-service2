a8c4fe492a751996174b1be6d9464e8b3d38853a
bhetalam.bhaskar <bhetalam.bhaskar@lge.com>
2020-04-02 11:54:43 +0530

    Support LS2 for 64-bit arch
    :Release Notes:
    64bit support in ls2
    
    :Detailed Notes:
    Fixed crash in ls2 to support 64bit
    
    :Testing Performed:
    Build Successful.
    UT Passed.
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-101759]: Migrate ls2 fixes to support 64-bit
    
    Change-Id: I4732f7876d3a7e915dffd16403104a008a521a18
    Reviewed-on: http://gpro.lge.com/c/webos/luna-service2/+/272590
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Nilesh Patil <nilesh.patil@lge.com>
    Reviewed-by: Rama Krishna <rama.krishna@lge.com>
    Tested-by: Rama Krishna <rama.krishna@lge.com>

--- src/libluna-service2/transport.h
+++ src/libluna-service2/transport.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2008-2018 LG Electronics, Inc.
+// Copyright (c) 2008-2021 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -189,6 +189,9 @@ void LSTransportTrustLevelGroupBitmaskFree(LSTransportTrustLevelGroupBitmask *v)
 size_t LSTransportGetSecurityMaskSize(_LSTransport *transport);
 GSList *LSTransportGetCategoryGroups(_LSTransport *transport);
 jvalue_ref LSTransportGetGroupsFromMask(_LSTransport *transport, LSTransportBitmaskWord *mask);
+
+jvalue_ref LSTransportGetTrustFromMask(_LSTransport *transport, LSTransportBitmaskWord *mask);
+GSList *LSTransportGetTrustLevelToGroups(_LSTransport *transport);
 
 #ifdef LS_TRACK_MESSAGE
 jvalue_ref LSTransportGetConnections(_LSTransport *transport);
